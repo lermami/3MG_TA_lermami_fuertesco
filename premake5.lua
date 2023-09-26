68c5c83ae13a4a33214bfcfb37324b606191d7f4
lermami <lermami@esat-libertad.com>
2023-09-26 20:28:41 +0200

    Complete conan

--- premake5.lua
+++ premake5.lua
@@ -1,31 +1,108 @@
 -- premake5.lua
 
-include("conanbuildinfo.premake.lua")
+conan = {}
+configs = {'Debug','Release','RelWithDebInfo'}
+for i = 1,3 do
 
-workspace "HelloWorld"
-   configurations { "Debug", "Release", "RelWithDebInfo" }
-   location "build"
-   conan_basic_setup()
+include("build/deps/"..configs[i].."/conanbuildinfo.premake.lua")
+  conan[configs[i]] = {}
+  local cfg = conan[configs[i]]
+  cfg["build_type"] = conan_build_type
+  cfg["arch"] = conan_arch
+  cfg["includedirs"] = conan_includedirs
+  cfg["libdirs"] = conan_libdirs
+  cfg["bindirs"] = conan_bindirs
+  cfg["libs"] = conan_libs
+  cfg["system_libs"] = conan_system_libs
+  cfg["defines"] = conan_defines
+  cfg["cxxflags"] = conan_cxxflags
+  cfg["cflags"] = conan_cflags
+  cfg["sharedlinkflags"] = conan_sharedlinkflags
+  cfg["exelinkflags"] = conan_exelinkflags
+  cfg["frameworks"] = conan_frameworks
 
-project "HelloWorld"
-   kind "ConsoleApp"
-   language "C++"
-   targetdir "bin/%{cfg.buildcfg}"
-   location "build"
+end
 
-   linkoptions { conan_exelinkflags }
+function conan_config_exec()
+  configs = {'Debug','Release','RelWithDebInfo'}
+    for i = 1,3 do
 
-   files { "**.h", "**.cc" }
+    local cfg = conan[configs[i]]
+    filter("configurations:"..configs[i])
 
-   filter "configurations:Debug"
-      defines { "Debug" }
-      symbols "On"
+      linkoptions { cfg["exelinkflags"] }
+      includedirs{ cfg["includedirs"] }
+      libdirs{ cfg["libdirs"] }
+      links{ cfg["libs"] }
+      links{ cfg["system_libs"] }
+      links{ cfg["frameworks"] }
+      defines{ cfg["defines"] }
 
-   filter "configurations:Release"
-      defines { "Release" }
-      optimize "On"
+     filter{}
+  end
+end
 
-    filter "configurations:RelWithDebInfo"
-      defines { "RelWithDebInf" }
-      optimize "On"
-      symbols "On"
+function conan_config_lib()
+  configs = {'Debug','Release','RelWithDebInfo'}
+  for i = 1,3 do
+    local cfg = conan[configs[i]]
+    filter("configurations:"..configs[i])
+      linkoptions { cfg["sharedlinkflags"] }
+      includedirs{ cfg["includedirs"] }
+      defines{ cfg["defines"] }
+    filter{}
+  end
+end
+
+workspace "Motor"
+  configurations { "Debug", "Release", "RelWithDebInfo" }
+  architecture "x64"
+  location "build"
+  cppdialect "c++20"
+  startproject "Window"
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+    runtime "Debug"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+    runtime "Release"
+
+  filter "configurations:RelWithDebInfo"
+    defines { "NDEBUG" }
+    optimize "On"
+    runtime "Release"
+    symbols "On"
+  filter {}
+
+project "Motor"
+  kind "StaticLib"
+  targetdir "build/%{cfg.buildcfg}"
+  includedirs "include"
+  conan_config_lib()
+  pchheader "stdafx.hpp"
+  pchsource "src/stdafx.cpp"
+  forceincludes { "stdafx.hpp" }
+
+  files {
+    "premake5.lua",
+    "src/build/conanfile.txt",
+    "src/build/conan.lua",
+    "src/stdafx.cpp", "src/stdafx.hpp",
+    "src/Window.cpp", "include/tija/Window.hpp",
+  }
+
+project"Window"
+  kind "WindowedApp"
+  language "C++"
+  targetdir "build/%{prj.name}/%{cfg.buildcfg}"
+  includedirs "include"
+  links "Tija"
+  conan_config_exec("Debug")
+  conan_config_exec("Release")
+  conan_config_exec("RelWithDebInfo")
+  debugargs { _MAIN_SCRIPT_DIR .. "/examples/data" }
+  files "examples/window.cpp"
