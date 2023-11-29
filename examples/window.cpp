ce54e56854084bd6d668a39fc06cc8d127f9239a
fuertesco <fuertesco@esat-alumni.com>
2023-11-29 19:02:40 +0100

    Created Window project

--- examples/window.cpp
+++ examples/window.cpp
@@ -1,16 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <optional>
-#include <string>
-
 #include "Window.hpp"
 #include "Engine.hpp"
-#include "Input.hpp"
-#include "geometry_test.hpp"
-#include "shader_management.hpp"
-#include "thread_manager.hpp"
-#include "buffer.hpp"
 
 int main(int, char**) {
   Engine e;
@@ -23,48 +15,10 @@ int main(int, char**) {
 
   if(glewInit() != GLEW_OK) return -1;
 
-  Triangle t("../include/test.vs", "../include/test.fs");
-
-  Input inputMap(w);
-
   while (!w.is_done()) {
     w.calculateLastTime();
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //Inputs
-    inputMap.updateInputs();
-
-    if (inputMap.IsKeyPressed('W')) {
-      //t.move(0.0f, 1.0f * w.getDeltaTime(), 0.0f);
-      t.addSize(0.01f, 0.01f);
-    }
-
-    if (inputMap.IsKeyPressed('A')) {
-      //t.move(-1.0f * w.getDeltaTime(), 0.0f, 0.0f);
-      t.roll(-0.01f);
-    }
-
-    if (inputMap.IsKeyPressed('S')) {
-      //t.move(0.0f, -1.0f * w.getDeltaTime(), 0.0f);
-      t.addSize(-0.01f, -0.01f);
-    }
-
-    if (inputMap.IsKeyPressed('D')) {
-      //t.move(1.0f * w.getDeltaTime(), 0.0f, 0.0f);
-      t.roll(0.01f);
-    }
-
-    double scroll_x = 0, scroll_y = 0;
-    inputMap.getScroll(scroll_x, scroll_y);
-    if (scroll_y > 0) {
-      t.move(0.0f, 1.0f * w.getDeltaTime(), 0.0f);
-    }
-    if (scroll_y < 0) {
-      t.move(0.0f, -1.0f * w.getDeltaTime(), 0.0f);
-    }
-
-    // Draw triangle
-    t.render();
 
     w.swap();
 
