20d81baf49953432bb242e047f019dc55281af5b
lermami <lermami@esat-alumni.com>
2024-03-10 19:34:02 +0100

    comment headers

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -1,36 +1,133 @@
+/**
+ * @file buffer.hpp
+ * @brief Header file for the VertexBuffer and IndexBuffer classes.
+ *
+ * @defgroup Buffers Vertex and Index Buffers
+ * @brief This file defines classes for managing vertex and index buffers in OpenGL.
+ */
+
 #pragma once
 #include <memory>
 #include "enum.hpp"
 
+ /**
+  * @class VertexBuffer
+  * @brief This class represents a vertex buffer object (VBO) in OpenGL.
+  *
+  * The VertexBuffer class encapsulates the creation, binding, and data upload for a
+  *  vertex buffer object (VBO) in OpenGL. It's primarily used by the ResourceManager.
+  *
+  * @ingroup Buffers
+  */
 class VertexBuffer {
   friend class ResourceManager;
 public:
+  /**
+   * Destructor for the VertexBuffer class.
+   *
+   * Releases the underlying OpenGL vertex buffer object.
+   */
   ~VertexBuffer();
 
+  /**
+   * Binds the vertex buffer object for use in rendering or update values.
+   */
   void bind() const;
+
+  /**
+  * Unbinds the vertex buffer object, disabling its use in rendering.
+  */
   void unbind() const;
+
+  /**
+   * Retrieves the handle of the underlying OpenGL vertex buffer object.
+   *
+   * @return The unsigned integer handle representing the VBO.
+   */
   unsigned get() const;
+
+  /**
+   * Retrieves the number of vertices stored in the vertex buffer.
+   *
+   * @return The count of vertices in the VBO.
+   */
   unsigned getCount() const;
 
+  /**
+   * Uploads float attribute data to the vertex buffer.
+   *
+   * This method uploads a specific attribute to the vertex buffer, such as
+   *  position, color, or texture coordinates.
+   *
+   * @param id The location of the attribute in the vertex shader.
+   * @param size The number of components for the attribute.
+   * @param stride The byte offset between consecutive vertices in the buffer.
+   * @param offset The byte offset within each vertex for the specific attribute.
+   */
   void uploadFloatAttribute(unsigned int id, int size, int stride, void* offset);
 
 private:
+  /**
+   * Private constructor to create a vertex buffer from raw vertex data.
+   *
+   * @param vertices A pointer to the array of floating-point vertex data.
+   * @param size The number of vertices in the array.
+   */
   VertexBuffer(float* vertices, unsigned size);
   unsigned handle_;
   unsigned count_;
 };
 
+/**
+ * @class IndexBuffer
+ * @brief This class represents an index buffer object (IBO) in OpenGL.
+ *
+ * The IndexBuffer class encapsulates the creation, binding, and data usage of an
+ *  index buffer object (IBO) in OpenGL. It's primarily used by the ResourceManager.
+ *
+ * @ingroup Buffers
+ */
 class IndexBuffer {
   friend class ResourceManager;
 public:
+  /**
+   * Destructor for the IndexBuffer class.
+   *
+   * Releases the underlying OpenGL index buffer object.
+   */
   ~IndexBuffer();
 
+  /**
+  * Binds the index buffer object for use in rendering.
+  */
   void bind() const;
+
+  /**
+   * Unbinds the index buffer object, disabling its use in rendering.
+   */
   void unbind() const;
+
+  /**
+   * Retrieves the handle of the underlying OpenGL index buffer object.
+   *
+   * @return The unsigned integer handle representing the IBO.
+   */
   unsigned get() const;
+
+  /**
+  * Retrieves the number of indices stored in the index buffer.
+  *
+  * @return The count of indices in the IBO.
+  */
   unsigned getCount() const;
 
 private:
+  /**
+   * Private constructor to create an index buffer from raw index data.
+   *
+   * @param vertices A pointer to the array of unsigned integer index data.
+   * @param size The number of indices in the array.
+   */
   IndexBuffer(unsigned* vertices, unsigned size);
   unsigned handle_;
   unsigned count_;
--- include/shader_management.hpp
+++ include/shader_management.hpp
@@ -1,11 +1,67 @@
+/**
+ * @file ShaderLoader.hpp
+ * @brief Header file for shader loading functionality.
+ *
+ * @defgroup ShaderLoader Shader Loader
+ * @brief This file defines functions for loading and compiling shader programs.
+ */
+#pragma once
 #include <string>
 #include "vector_3.hpp"
 
 class Window;
 
+/**
+ * @brief Creates a shader object of the specified type.
+ *
+ * This function creates a shader object of the specified type (vertex shader or fragment shader).
+ *
+ * @param type The type of shader to create. 0 to vertex and 1 to fragment shader.
+ * @return The ID of the created shader object, or 0 if creation failed.
+ */
 unsigned int CreateShader(int type);
+
+/**
+ * @brief Compiles the shader source code for the specified shader object.
+ *
+ * This function compiles the provided shader source code for the shader object identified by ID.
+ *
+ * @param id The ID of the shader object to compile.
+ * @param src The shader source code as a string.
+ */
 void CompileShader(unsigned int id, const char* src);
+
+/**
+ * @brief Creates a shader program by linking a vertex shader and a fragment shader.
+ *
+ * This function links a vertex shader and a fragment shader to create a complete shader program.
+ *  The program is stored in a internal list of the specified window.
+ *
+ * @param w Reference to the Window object.
+ * @param vs The vertex shader source code as a C-style string.
+ * @param fs The fragment shader source code as a C-style string.
+ * @return The ID of the created shader program, or 0 if linking failed.
+ */
 unsigned int CreateProgram(Window& w, const char* vs, const char* fs);
+
+/**
+ * @brief Reads the contents of a file into a string.
+ *
+ * This function reads the contents of a file specified by path and returns the contents as a string.
+ *
+ * @param file The path to the file to read.
+ * @return The contents of the file as a string, or an empty string if reading failed.
+ */
 std::string ReadFiles(const std::string& file);
 
+/**
+ * @brief Sets a vec3 uniform variable in a shader program.
+ *
+ * This function sets a uniform variable of type vec3 in a shader program.
+ *  The name of the uniform variable and the vector value are provided as arguments.
+ *
+ * @param program The ID of the shader program to set the uniform in.
+ * @param name The name of the uniform variable to set.
+ * @param vector The vec3 value to set the uniform variable to.
+ */
 void SetVector3(unsigned int program, char* name, Vec3 vector);
